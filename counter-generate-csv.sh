c91a3e2f994510fb6710ac9f364df46621d14b23
Jean-Marc Desprez <jmdesprez@cloudbees.com>
2022-09-13 21:56:32 +0200

    Remove `no_api` (#2)

--- counter-generate-csv.sh
+++ counter-generate-csv.sh
@@ -31,29 +31,25 @@ label_invalid_regex='\binvalid\b'
 # csv files
 current_time=$(date "+%Y%m%d-%H%M%S")
 filename="hacktoberfest_$current_time.csv"
-# do not request GH api, use only the existing files
-no_api=false
 ##
 
 getOrganizationData() {
   local org="$1"
   local json_filename="$org"
 
-  if ! $no_api; then
-    rm "$json_filename"*.json
-    local url_encoded_query
-    url_encoded_query=$(jq --arg query "org:$org $query" --raw-output --null-input '$query|@uri')
-    local page=1
-    while true; do
-      echo "org: $org get page $page"
-      gh api -H "Accept: application/vnd.github+json" "/search/issues?q=$url_encoded_query&sort=updated&order=desc&per_page=100&page=$page" >"$json_filename$page.json"
-      # less accurate, can make 1 useless call if the number of issues is a multiple of 100
-      if test "$(jq --raw-output '.items|length' "$json_filename$page.json")" -ne 100; then
-        break
-      fi
-      ((page++))
-    done
-  fi
+  rm "$json_filename"*.json
+  local url_encoded_query
+  url_encoded_query=$(jq --arg query "org:$org $query" --raw-output --null-input '$query|@uri')
+  local page=1
+  while true; do
+    echo "org: $org get page $page"
+    gh api -H "Accept: application/vnd.github+json" "/search/issues?q=$url_encoded_query&sort=updated&order=desc&per_page=100&page=$page" >"$json_filename$page.json"
+    # less accurate, can make 1 useless call if the number of issues is a multiple of 100
+    if test "$(jq --raw-output '.items|length' "$json_filename$page.json")" -ne 100; then
+      break
+    fi
+    ((page++))
+  done
 
   jq --arg org "$org" --arg accepted_arg "$label_accepted" --arg spam "$label_spam_regex" --arg invalid "$label_invalid_regex" --raw-output --slurp --from-file json_to_csv.jq "$json_filename"*.json >>"$filename"
 }
