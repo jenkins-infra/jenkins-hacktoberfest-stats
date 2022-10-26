ffa11abb55a1a4abeab21240e6958371f3f0859c
Jean-Marc MEESSEN <jean-marc@meessen-web.org>
2022-10-26 15:59:47 +0200

    implement a PR creation date search range (#15)

--- counter-generate-csv.sh
+++ counter-generate-csv.sh
@@ -28,8 +28,9 @@ fi
 
 ### Setting up constants
 
-# We exclude the PRs created by dependabot and renovate
-query='is:pr -author:app/dependabot -author:app/renovate created:>2022-10-01'
+# We exclude the PRs created by dependabot and renovate and those created outside the period
+hacktoberfest_year="2022"
+query="is:pr -author:app/dependabot -author:app/renovate created:${hacktoberfest_year}-10-01..${hacktoberfest_year}-10-31"
 
 #Spec: is "hacktoberfest" flag set? 
 label_hacktoberfest='\bhacktoberfest\b'
