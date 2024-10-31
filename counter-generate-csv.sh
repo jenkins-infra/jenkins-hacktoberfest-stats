924950b30ddaed3bd660b8c7486f8d2ee9908a61
gounthar <gounthar@gmail.com>
2024-10-31 20:09:39 +0100

    fix(scripts): Adds jenkins-docs organisation and comments.

--- counter-generate-csv.sh
+++ counter-generate-csv.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 ######
-# this script retrieves informations from GitHub to track Hacktoberfest participation in the Jenkins project
-# The resulting data is enriched and made available as a CSV
-###### 
+# This script retrieves information from GitHub to track Hacktoberfest participation in the Jenkins project.
+# The resulting data is enriched and made available as a CSV.
+######
 
 set -e
 
-# check wether required tools are available
+# Check whether required tools are available
 if ! command -v "gh" >/dev/null 2>&1
 then
   echo "ERROR: command line 'gh' required but not found. Exiting."
@@ -29,20 +29,19 @@ fi
 ### Setting up constants
 
 # We exclude the PRs created by dependabot and renovate and those created outside the period
-hacktoberfest_year="2023"
+hacktoberfest_year=$(date "+%Y")
 query="is:pr -author:app/dependabot -author:app/renovate created:${hacktoberfest_year}-10-01..${hacktoberfest_year}-10-31"
 
-#Spec: is "hacktoberfest" flag set? 
+# Spec: is "hacktoberfest" flag set?
 label_hacktoberfest='\bhacktoberfest\b'
 
 # Spec: Is flag “Hacktoberfest-approved” set? (case insensitive)
 label_accepted='Hacktoberfest-accepted, Hacktoberfest-approved'
-#  - Additional labels should be reported in the result (true/false): spam, invalid
+# Additional labels should be reported in the result (true/false): spam, invalid
 label_spam_regex='\bspam\b'
-#  - Additional labels should be reported in the result (true/false): spam, invalid
 label_invalid_regex='\binvalid\b'
 
-# csv files
+# CSV files
 current_time=$(date "+%Y%m%d-%H%M%S")
 data_filename_root="data/hacktoberfest"
 raw_csv_filename="${data_filename_root}_raw_${current_time}.csv"
@@ -54,21 +53,20 @@ summaryFileContribs_latest="${data_filename_root}_contributors_latest.csv"
 # CSV file with the hacktoberfest repositories
 repos_csv_file="repo_data/hacktoberfest_repos_latest.csv"
 
-# create the data directory if it doesn't exist
+# Create the data directory if it doesn't exist
 [ -d data ] || mkdir data
 [ -d json_data ] || mkdir json_data
 ##
-
 
 # Function to facilitate checking whether a PR is complete and acceptable for Hacktoberfest
 isAccepted() {
   local merge_date="$2"
   local hacktoberfest_approved="$1"
 
-  #is the merge_date empty?
+  # Is the merge_date empty?
   if [ -z "$merge_date" ]
   then
-    # it has not been merged
+    # It has not been merged
     if test "$hacktoberfest_approved" == "true"
     then
       echo "1"
@@ -76,7 +74,7 @@ isAccepted() {
       echo "0"
     fi
   else
-    # the PR has been merged
+    # The PR has been merged
     echo "1"
   fi
 }
@@ -97,7 +95,6 @@ lookupHacktoberfestTopic() {
     if test "$(grep -c "/${trimmed_repository}\"" "${repos_csv_file}")" -eq 1
     then
       is_hacktoberfest_repo="true"
-      #echo "${trimmed_repository} - ${url}"
       hacktoberfest_complete=$(isAccepted $approved $merged_at )
       echo "$org,$repository,$is_hacktoberfest_repo,$url,$state,$created_at,$merged_at,$user_login,$is_hacktoberfest_labeled,$approved,$spam,$invalid,$hacktoberfest_complete,$title" >> "${output_csv_file}"
     else
@@ -107,13 +104,14 @@ lookupHacktoberfestTopic() {
       then
         hacktoberfest_complete=$(isAccepted $approved $merged_at)
         echo "$org,$repository,$is_hacktoberfest_repo,$url,$state,$created_at,$merged_at,$user_login,$is_hacktoberfest_labeled,$approved,$spam,$invalid,$hacktoberfest_complete,$title" >> "${output_csv_file}"
-      fi 
+      fi
     fi
   done < <(tail -n +2 ${raw_csv_file})
 }
 
 # Function that retrieves and processes the GitHub information for a given organization
 getOrganizationData() {
+  echo "Getting org data for $1"
   local org="$1"
   local json_filename="json_data/${org}"
 
@@ -124,12 +122,12 @@ getOrganizationData() {
   while true; do
     echo "org: $org get page $page"
     gh api -H "Accept: application/vnd.github+json Retry-After: 30" "/search/issues?q=$url_encoded_query&sort=updated&order=desc&per_page=100&page=$page" >"$json_filename$page.json"
-    # less accurate, can make 1 useless call if the number of issues is a multiple of 100
+    # Less accurate, can make 1 useless call if the number of issues is a multiple of 100
     if test "$(jq --raw-output '.items|length' "$json_filename$page.json")" -ne 100; then
       break
     fi
     ((page++))
-    # Dirty trick to a avoid hitting secondary rate limit.
+    # Dirty trick to avoid hitting secondary rate limit.
     sleep 15
   done
 
@@ -139,36 +137,36 @@ getOrganizationData() {
 # Spec: Produce a CSV list of PRs with following details: PR URL, PR Title, Repository, Status (Open, Merged), Creation date, Merge date (if applicable), PR Author, Is flag “Hacktoberfest-approved” set?
 echo 'org,repository,url,state,created_at,merged_at,user.login,is_hacktoberfest_labeled,approved,spam,invalid,title' >"$raw_csv_filename"
 
-# seems not possible to query both org at the same time
-# Spec: PRs in all repositories of jenkinsci and jenkins-infra
+# Seems not possible to query both org at the same time
+# Spec: PRs in all repositories of j
+#getOrganizationData jenkins-docs, enkinsci and jenkins-infra
+getOrganizationData jenkins-docs
 getOrganizationData jenkinsci
 getOrganizationData jenkins-infra
 
-# # Update the list of participating repositories (only if it doesn't exist)
+# Update the list of participating repositories (only if it doesn't exist)
 if [ ! -f ${repos_csv_file} ]
 then
   ./hacktoberfest-repositories.sh
 fi
 
-
 # Look through the generated file and lookup the repositories that have the hacktoberfest topic
-lookupHacktoberfestTopic ${raw_csv_filename} ${csv_filename} 
+lookupHacktoberfestTopic ${raw_csv_filename} ${csv_filename}
 
-#https://medium.com/clarityai-engineering/back-to-basics-how-to-analyze-files-with-gnu-commands-fe9f41665eb3
+# https://medium.com/clarityai-engineering/back-to-basics-how-to-analyze-files-with-gnu-commands-fe9f41665eb3
 # awk -F'"' -v OFS='"' '{for (i=2; i<=NF; i+=2) {gsub(",", "", $i)}}; $0' hacktoberfest_20220928-162143.csv
 awk -F'"' -v OFS='"' '{for (i=2; i<=NF; i+=2) {gsub(",", "", $i)}}; $0' $csv_filename | datamash -t, --sort --headers groupby 8 sum 13 > "$summaryFileContribs"
 echo "----------------------------------------"
 cat $summaryFileContribs
 
-# update the latest
+# Update the latest
 cp ${csv_filename} ${csv_filename_latest}
 cp $summaryFileContribs $summaryFileContribs_latest
-
 
 echo "----------------------------------------"
 echo " SUMMARY"
 echo "----------------------------------------"
-# total not automated PRs in Jenkinsci and Jenkin-infra organisation
+# Total not automated PRs in Jenkinsci and Jenkin-infra organisation
 wrk_raw_pr=$(cat ${raw_csv_filename} | wc -l)
 raw_pr=$(echo "${wrk_raw_pr}" | xargs)
 echo "Total number of PRs created in jenkinsci and jenkins-infra orgs: ${raw_pr}"
